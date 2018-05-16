76ecbd51902ed8fd04e8a7a4da346e53f21a58e5
Nate Shirey <nateshirey6@gmail.com>
2018-05-16 12:33:26 -0600

    Updates to grappling hook
    added functionality of distance joint 2d. janky but working

--- charactercontrollerpractice/Assets/2DPlatformer/Scripts/GrapplingHookv2.cs
+++ charactercontrollerpractice/Assets/2DPlatformer/Scripts/GrapplingHookv2.cs
@@ -11,66 +11,59 @@ public class GrapplingHookv2 : MonoBehaviour
 	private bool inRange;
 
 	private Vector3 target;
+    private Rigidbody2D targetRb2d;
 	private Vector3 hookLocation;
 	public float hookSpeed;
+
+    private DistanceJoint2D joint;
 
 	// Use this for initialization
 	void Start()
 	{
-
 		inRange = false;
+        joint = this.GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
 
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
 		hookLocation = hook.transform.position;
+        joint.enabled = false;
 
         if (Input.GetKey("e") && inRange == true)
         {
-          
+            joint.connectedBody = targetRb2d;
+            joint.connectedAnchor = target;
+            joint.enabled = true;
             sendHook();
-        
         }
-
         else if (hookLocation != this.transform.position)
         {
-
             returnHook();
-
         }
-
 	}
  
 	void sendHook()
 	{
-
            hook.transform.position = Vector2.MoveTowards(hookLocation, target, hookSpeed * Time.deltaTime);
            Debug.Log(hookLocation);
-
 	}
 
     void returnHook()
     {
-
         hook.transform.position = Vector2.MoveTowards(hookLocation, this.transform.position, hookSpeed * Time.deltaTime);
-
     }
 
 	void OnTriggerStay2D(Collider2D col)
 	{
-        
 		if(col.gameObject.tag == "Grapple Point")
 		{
-
 			target = col.gameObject.transform.position;
-			inRange = true;
+            targetRb2d = col.GetComponent<Rigidbody2D>();
+            inRange = true;
 			Debug.Log("in range");
-
 		}
-
 	}
-
 }
--- charactercontrollerpractice/Assets/2DPlatformer/Scripts/PlayerPlatformerController.cs
+++ charactercontrollerpractice/Assets/2DPlatformer/Scripts/PlayerPlatformerController.cs
@@ -5,83 +5,52 @@ using UnityEngine;
 public class PlayerPlatformerController : PhysicsObject
 {
 
-	public float maxSpeed = 7;
+    public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
 
-	private SpriteRenderer spriteRenderer;
-	private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     private bool attatched;
     public float swingSpeed;
 
-    //Grappling hook variables
-    public GameObject hook;
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
 
-    private bool isHooked;
-    private bool inRange;
-
-    private Vector3 target;
-    private Vector3 hookLocation;
-    public float hookSpeed;
-
-
-	// Use this for initialization
-	void Awake()
-	{
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		animator = GetComponent<Animator>();
-        inRange = false;
-	}
-
-	protected override void ComputeVelocity ()
-	{
+    protected override void ComputeVelocity()
+    {
         //control player movement
-        Vector2 move = Vector2.zero;
+        Vector2 groundMove = Vector2.zero;
 
 
-        move.x = Input.GetAxis("Horizontal");
+        groundMove.x = Input.GetAxis("Horizontal");
 
 
-		if (Input.GetButtonDown ("Jump") && grounded)
-		{
-			velocity.y = jumpTakeOffSpeed;
-		}
-		else if (Input.GetButtonUp ("Jump"))
-		{
-			if (velocity.y > 0)
-				velocity.y = velocity.y * .5f;
-		}
-
-		bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-		if (flipSprite) 
-		{
-			spriteRenderer.flipX = !spriteRenderer.flipX;
-		}
-
-		animator.SetBool("grounded", grounded);
-		animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
-
-		targetVelocity = move * maxSpeed;
-
-	}
-
-    void sendHook()
-    {
-        hook.transform.position = Vector2.MoveTowards(hookLocation, target, hookSpeed * Time.deltaTime);
-    }
-
-    void returnHook()
-    {
-        hook.transform.position = Vector2.MoveTowards(hookLocation, this.transform.position, hookSpeed * Time.deltaTime);
-    }
-
-    void OnTriggerStay2D(Collider2D col)
-    {
-
-        if (col.gameObject.tag == "Grapple Point")
+        if (Input.GetButtonDown("Jump") && grounded)
         {
-            target = col.gameObject.transform.position;
-            inRange = true;
+            velocity.y = jumpTakeOffSpeed;
         }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+                velocity.y = velocity.y * .5f;
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (groundMove.x > 0.01f) : (groundMove.x < 0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = groundMove * maxSpeed;
+
     }
+
 }
